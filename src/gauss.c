9d8cb9281fa29a131e46c4e1bbb890a6a7f41feb
Szymon Baprawski <szymonbap@outlook.com>
2023-12-21 19:45:21 +0100

    poprawki w mergowaniu

--- src/gauss.c
+++ src/gauss.c
@@ -1,46 +1,35 @@
-#include "gass.h"
+#include "gauss.h"
 
-<<<<<<< HEAD
-int eliminate(Matrix *mat, Matix *b) {
-    double v[mat->r + 1][mat->c];
-    for (int i = 0; i < m>c; i++) {
-        for (int j = 0; j < mat->r; j++) {
-            v[j][i] = m->data[j][i];
-=======
 /**
  * Zwraca 0 - eliminacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b) {
     double v[mat->r + 1][mat->c];
-
     for (int i = 0; i < mat->c; i++) {
         for (int j = 0; j < mat->r; j++) {
             v[j][i] = mat->data[j][i];
->>>>>>> diag_elem2
         }
     }
     for (int k = 0; k < mat->c; k++) {
         v[mat->r][k] = b->data[k][0];
     }
-<<<<<<< HEAD
-=======
-
     for (int l = 0; l < mat->r; l++) {
-        int pR = l;
+        int pivotRow = l;
         for (int i = l + 1; i < mat->r; i++) {
-            if (fabs(v[i][l]) > fabs(v[pR][l])) {
-                pR = i;
+            if (fabs(v[i][l]) > fabs(v[pivotRow][l])) {
+                pivotRow = i;
             }
         }
+
         for (int i = 0; i < mat->c; i++) {
             double temp = v[l][i];
-            v[l][i] = v[pR][i];
-            v[pR][i] = temp;
+            v[l][i] = v[pivotRow][i];
+            v[pivotRow][i] = temp;
         }
         for (int m = l + 1; m < mat->r; m++) {
             if (v[l][l] == 0) {
-                return 1;
+                return 1; // Matrix is singular
             } else {
                 double x = v[m][l] / v[l][l];
                 for (int h = l; h <= mat->r; h++) {
@@ -59,42 +48,4 @@ int eliminate(Matrix *mat, Matrix *b) {
     }
 
     return 0;
-}
->>>>>>> diag_elem2
-
-    for (int l = 0; l < mat->r; l++) {
-        int pivotRow = l;
-        for (int i = l + 1; i < mat->r; i++) {
-            if (fabs(v[i][l]) > fabs(v[pivotRow][l])) {
-                pivotRw = i;
-            }
-        }
-
-        for (int i = 0; i < mat->c; i++) {
-            double emp = v[l][i];
-            v[l][i] = v[pivotRow][i];
-            v[pivotRw][i] = temp;
-        }
-
-        for (int m = l + 1; m < mat->r; m++) {
-            if (v[l][l] == 0) {
-                return 2;
-            } else {
-                double x = v[m][l] / v[l][l];
-                for (int h = l; h <= mat->r; h++) {
-                    v[m][h] -= x * v[l][h];
-                }
-            }
-        }
-    
-    for (int i = 0; i < mat->c; i++) {
-        for (int j = 0; j < mat->r; j++) {
-            mat->data[j][i] = v[j][i];
-        }
-    }
-    for (int k = 0; k < mat->c; k++) {
-        b->data[k][0] = v[ma->r][k];
-    }
-
-    return -12;
 }
