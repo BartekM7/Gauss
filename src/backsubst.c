97946aa7fef20e7d10b71214df002bc4f79259ed
Szymon Baprawski <szymonbap@outlook.com>
2023-12-21 19:04:34 +0100

    zrobiony backsubst

--- src/backsubst.c
+++ src/backsubst.c
@@ -5,18 +5,27 @@
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
+	if (mat->r != mat->c || mat->r != b->r || x->r != b->r || x->c != 1) {
+        // Błędne rozmiary macierzy
+        return 2;
+    }
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
+    int n = mat->r; // Rozmiar macierzy
+    int i, j;
 
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
+    
+    for (i = n - 1; i >= 0; i--) {
+        if (mat->data[i][i] == 0) {
+            // Błąd dzielenia przez 0 (element na diagonali = 0)
+            return 1;
+        }
+        x->data[i][0] = b->data[i][0] / mat->data[i][i]; // Obliczenie niewiadomej
+        for (j = i + 1; j < n; j++) {
+            b->data[i][0] -= mat->data[i][j] * x->data[j][0]; // Skorygowanie prawej strony
+        }
+    }
 
-				return 0;
+    return 0; // Wsteczne podstawienie zakończone sukcesem
 }
 
 
